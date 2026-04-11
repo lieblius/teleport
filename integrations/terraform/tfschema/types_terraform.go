3e07b782641c450a8857833cfd3d8e25672af3ec
Jake Ward <jacob.ward@goteleport.com>
2026-04-11 15:50:08 +0100

    Exclude client_secret from Terraform schema

--- integrations/terraform/tfschema/types_terraform.go
+++ integrations/terraform/tfschema/types_terraform.go
@@ -4874,6 +4874,7 @@ func GenSchemaSAMLConnectorV2(ctx context.Context) (github_com_hashicorp_terrafo
 						"client_secret": {
 							Description: "ClientSecret is the client secret to use for OAuth client credentials grant.",
 							Optional:    true,
+							Sensitive:   true,
 							Type:        github_com_hashicorp_terraform_plugin_framework_types.StringType,
 						},
 					}),
