5d563faf5d82e5f7a2e8a360dd7e9a34f5a20671
Ryan Hammonds <ryan.hammonds@goteleport.com>
2026-04-15 16:19:44 -0400

    Fix tests after refactoring send/receive handlers

--- lib/srv/desktop/rdp/rdpclient/client_nop.go
+++ lib/srv/desktop/rdp/rdpclient/client_nop.go
@@ -39,7 +39,7 @@ type Client struct {
 // New creates and connects a new Client based on opts.
 //
 //nolint:staticcheck // SA4023. False positive, depends on build tags.
-func New(_ *tdp.Conn, cfg Config) (*Client, error) {
+func New(_ *tdp.Conn, _ *tdpb.Hello, cfg Config) (*Client, error) {
 	return nil, errors.New("the real rdpclient.Client implementation was not included in this build")
 }
 
